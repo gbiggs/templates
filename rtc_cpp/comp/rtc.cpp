40982b3c610cc2766ebe6ffc4583354765c44c9d
Geoffrey Biggs <geoffrey.biggs@aist.go.jp>
2010-11-15 14:55:43 +0900

    RT-Component template

---
+++ rtc_cpp/comp/rtc.cpp
@@ -0,0 +1,105 @@
+/* RTC:
+ *
+ * Copyright (C) 2009-2010
+ *     Geoffrey Biggs
+ *     RT-Synthesis Research Group
+ *     Intelligent Systems Research Institute,
+ *     National Institute of Advanced Industrial Science and Technology (AIST),
+ *     Japan
+ *     All rights reserved.
+ * Licensed under the Eclipse Public License -v 1.0 (EPL)
+ * http://www.opensource.org/licenses/eclipse-1.0.txt
+ *
+ * Component source file.
+ */
+
+
+#include "rtc.h"
+
+
+RTC::RTC(RTC::Manager* manager)
+    : RTC::DataFlowComponentBase(manager),
+    _port_("<PORT_NAME>", _),
+    //svc_prov_(),
+    //svc_port_("<PORT_NAME>")
+{
+}
+
+
+RTC::~RTC()
+{
+}
+
+
+RTC::ReturnCode_t RTC::onInitialize()
+{
+    bindParameter("", _, "");
+    /*std::string active_set =
+        m_properties.getProperty("configuration.active_config", "default");
+    m_configsets.update(active_set.c_str());*/
+
+    comp.addInPort(_port_.getName(), _port_);
+    comp.addOutPort(_port_.getName(), _port_);
+    //svc_port_.registerProvider("<INSTANCE_NAME>", "<TYPE_NAME>", svc_prov_);
+    //comp.addPort(svc_port_);
+
+    return RTC::RTC_OK;
+}
+
+
+/*RTC::ReturnCode_t RTC::onFinalize()
+{
+    return RTC::RTC_OK;
+}*/
+
+
+/*RTC::ReturnCode_t RTC::onActivated(RTC::UniqueId ec_id)
+{
+    return RTC::RTC_OK;
+}*/
+
+
+/*RTC::ReturnCode_t RTC::onDeactivated(RTC::UniqueId ec_id)
+{
+    return RTC::RTC_OK;
+}*/
+
+
+/*RTC::ReturnCode_t RTC::onExecute(RTC::UniqueId ec_id)
+{
+
+    return RTC::RTC_OK;
+}*/
+
+
+static const char* spec[] =
+{
+    "implementation_id", "<COMP>",
+    "type_name",         "<COMP_TYPE>",
+    "description",       "",
+    "version",           "1.0",
+    "vendor",            "Geoffrey Biggs, AIST",
+    "category",          "",
+    "activity_type",     "PERIODIC",
+    "kind",              "DataFlowComponent",
+    "max_instance",      "1",
+    "language",          "C++",
+    "lang_type",         "compile",
+    // Configuration variables
+    // Widget
+    //"conf.__widget__.", "text",
+    //"conf.__widget__.", "spin",
+    // Constraints
+    ""
+};
+
+extern "C"
+{
+    void rtc_init(RTC::Manager* manager)
+    {
+        coil::Properties profile(spec);
+        manager->registerFactory(profile, RTC::Create<RTC>,
+                RTC::Delete<RTC>);
+    }
+};
+
---
+++ rtc_cpp/comp/rtc.h
@@ -0,0 +1,69 @@
+/* RTC:
+ *
+ * Copyright (C) 2009-2010
+ *     Geoffrey Biggs
+ *     RT-Synthesis Research Group
+ *     Intelligent Systems Research Institute,
+ *     National Institute of Advanced Industrial Science and Technology (AIST),
+ *     Japan
+ *     All rights reserved.
+ * Licensed under the Eclipse Public License -v 1.0 (EPL)
+ * http://www.opensource.org/licenses/eclipse-1.0.txt
+ *
+ * Component header file.
+ */
+
+
+#ifndef RTC_H__
+#define RTC_H__
+
+#include <rtm/Manager.h>
+#include <rtm/DataFlowComponentBase.h>
+//#include <rtm/InPort.h>
+//#include <rtm/OutPort.h>
+//#include <rtm/CorbaPort.h>
+//#include "service_impl.h"
+
+using namespace RTC;
+
+
+// Base exception
+class BaseRTCError : public std::runtime_error
+{
+    public:
+        BaseRTCError(const std::string &arg)
+            : std::runtime_error(std::string("Base error ") + arg)
+        {}
+};
+
+
+class RTC
+: public RTC::DataFlowComponentBase
+{
+    public:
+        RTC(RTC::Manager* manager);
+        ~RTC();
+
+        virtual RTC::ReturnCode_t onInitialize();
+        //virtual RTC::ReturnCode_t onFinalize();
+        //virtual RTC::ReturnCode_t onActivated(RTC::UniqueId ec_id);
+        //virtual RTC::ReturnCode_t onDeactivated(RTC::UniqueId ec_id);
+        //virtual RTC::ReturnCode_t onExecute(RTC::UniqueId ec_id);
+
+    private:
+        RTC:: _;
+        RTC::InPort<RTC::> _port_;
+        RTC:: _;
+        RTC::OutPort<RTC::> _port_;
+        //ServiceProvider svc_prov_;
+        //RTC::CorbaPort svc_port_;
+};
+
+
+extern "C"
+{
+    DLL_EXPORT void rtc_init(RTC::Manager* manager);
+};
+
+#endif // RTC_H__
+
